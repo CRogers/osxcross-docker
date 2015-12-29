e3aa8c3ec9567c0f30adb41f85ea791071929397
Callum Rogers <callumrogers@hotmail.co.uk>
2015-12-29 02:11:24 +0000

    Uncomment out lines

--- build.sh
+++ build.sh
@@ -1,11 +1,11 @@
-#!/bin/sh
+#!/bin/bash
 
 if [[ -z "$JOBS" ]]; then
   JOBS=$(nproc || echo 2)
 fi
 
-#docker build -t osxcross-docker docker-build
-#docker run -it -e "JOBS=$JOBS" -v "$(pwd)/out:/osxcross/target" osxcross-docker /osxcross/build-everything.sh
+docker build -t osxcross-docker docker-build
+docker run -it -e "JOBS=$JOBS" -v "$(pwd)/out:/osxcross/target" osxcross-docker /osxcross/build-everything.sh
 
 echo "Archiving distribution output"
 tar c -C out . | xz -3 -zf - >osxcross-dist.tar.xz
